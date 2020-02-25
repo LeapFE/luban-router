9f7cae26d786ee42fc73ccbe436640bdfddb7e6b
front-end-captain <18810940932@163.com>
2020-02-25 22:51:39 +0800

    fix: dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 import { LubanRouter } from "./router";
 
+export {
+  Link,
+  NavLink,
+  BrowserRouter,
+  HashRouter,
+  // from react-router
+  MemoryRouter,
+  Prompt,
+  Redirect,
+  Route,
+  // Router,
+  StaticRouter,
+  Switch,
+  // context as __RouterContext,
+  generatePath,
+  matchPath,
+  useHistory,
+  useLocation,
+  useParams,
+  useRouteMatch,
+  withRouter,
+} from "react-router-dom";
+
 export { LubanRouter };
 
 export default LubanRouter;
